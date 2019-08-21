a6e46dcc51614df98d41b82f271a4064eaa262bf
rajorshiroy <35338951+rajorshiroy@users.noreply.github.com>
2019-08-21 17:49:26 +0530

    removed previous site

--- Maulana Abul Kalam Azad University of Technology_files/myscript.js
+++
@@ -1,9 +0,0 @@
-name = prompt("Enteter your name");
-name = name.toUpperCase();
-document.getElementById("student-name").textContent=name;
-
-reg = prompt("Enter Reg No. (Number only)");
-document.getElementById("student-reg").textContent=reg;
-
-roll = prompt("Enter your roll");
-document.getElementById("student-roll").textContent=roll;
